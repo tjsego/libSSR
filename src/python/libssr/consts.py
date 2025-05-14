e07da9c86448199ffc6d81668c03769c5982c9ef
tjsego <tjsego@gmail.com>
2025-05-14 05:15:33 -0400

    Removed numba platform restrictions

--- src/python/libssr/consts.py
+++ src/python/libssr/consts.py
@@ -1,5 +1,3 @@
-import sys
-
 DEF_EVAL_NUM = 100
 """Default number of transform variable evaluations"""
 
@@ -9,12 +7,8 @@ DEF_NUM_VAR_PERS = 5
 has_numba = False
 """Flag signifying whether numba is usable."""
 
-# Numba seems to only behave well on Windows
-if sys.platform.startswith('darwin') or sys.platform.startswith('linux'):
+try:
+    import numba
+    has_numba = True
+except ImportError:
     pass
-else:
-    try:
-        import numba
-        has_numba = True
-    except ImportError:
-        pass
